3194f68124be1e8195af296a18df5b9646bbcaf2
Nathan Duarte <nduartech101@gmail.com>
2020-01-18 13:34:31 -0500

    Done

--- jspark/src/main/java/Main.java
+++ jspark/src/main/java/Main.java
@@ -5,7 +5,8 @@ import org.apache.spark.sql.SparkSession;
 public class Main {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().appName("MVC").getOrCreate();
-        Dataset<Row> sqlDF = spark.sql("SELECT * FROM parquet.`mvc/crashes/` A, parquet.`mvc/vehicles/` B, parquet.`mvc/person/` C WHERE A.CRASH_DATE = B.CRASH_DATE AND B.CRASH_DATE = C.CRASH_DATE AND to_date(A.CRASH_DATE,'mm/dd/yyyy')>=to_date('04/01/2019','mm/dd/yyyy')");
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM parquet.`mvc/crashes/` A, parquet.`mvc/vehicles/` B, parquet.`mvc/person/` C WHERE A.CRASH_DATE = B.CRASH_DATE AND B.CRASH_DATE = C.CRASH_DATE AND to_date(A.CRASH_DATE,'mm/dd/yyyy')==to_date('06/23/2019','mm/dd/yyyy')");
+        sqlDF.printSchema();
         spark.stop();
     }
 }
